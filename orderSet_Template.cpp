abb8951bab990ed0a4c54ba805caf87488e6f519
MD. Delwar Jahan <119843029+delwar03@users.noreply.github.com>
2024-03-18 20:18:45 +0600

    Create orderSet_Template.cpp

---
+++ orderSet_Template.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T> using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// o_st.find_by_order(k) return kth element. 0 indexed.
+// o_st.order_of_key(k) returns count of elements strictly less than k.
+const int M = 1e9 + 7;
+const int N = 1e5 + 10;
+const int INF = 1e15 + 10;
+
+void solve() {
+    
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1, c = 1; cin>>t;
+    while(t--) {
+        // cout<<"Case "<<c++<<": "<<endl;
+        solve();
+    }
+}
+ 
+/*
+i/p:  
+o/p: 
+*/ 
